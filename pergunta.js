f86688af169e5c60c2a2642107168a86b059193f
joaocr7messineymar <144161833+joaocr7messineymar@users.noreply.github.com>
2024-11-29 07:55:26 -0300

    Add files via upload

---
+++ pergunta.js
@@ -0,0 +1,31 @@
+criaCartao (
+    'Geografia',
+    'Qual a capital da Argentina?',
+    'Buenos Aires'
+)
+
+criaCartao (
+    'Esporte',
+    'Em qual cidade foi realizada as olimpíadas de 2012?',
+    'Londres'
+)
+criaCartao (
+    'Matemática',
+    'O que é o Teorema de Pitágoras?',
+    'É uma expressão matemática que relaciona as medidas dos lados de um triângulo retângulo, conhecidos como catetos e hipotenusa.'
+)
+criaCartao(
+    'Química',
+    'Qual a famosa frase do químico francês Lavoisier?',
+    'Na natureza nada se cria, nada se perde, tudo se transforma.'
+)
+criaCartao (
+    'Biologia',
+    'O que é o Complexo de Golgi?',
+    'É uma organela celular que está relacionada com o processo de secreção de substâncias'
+)
+criaCartao (
+    'História',
+    'Qual foi o marco da Revolução Francesa?',
+    'Teve como grande marco a Queda da Bastilha, que aconteceu no dia 14 de julho de 1789, espalhou-se pela França e aconteceu entre 1789 e 1799'
+)
